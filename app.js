23811b52edeeb15548e89607b0ef347ad67931a7
Legend Andrews <139176918+Cantrell24@users.noreply.github.com>
2024-01-27 01:27:46 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const sum = (a,b) => {
+    return a + b}
+
+   
+
+    const fromYenToPound = function(valueInYen) {
+        // Convert the given valueInEuro to dollars
+        let valueInPound = valueInYen * 0.87;
+        // return the dollar value
+        return valueInPound;
+    }
+    module.exports = {sum, fromYenToPound};
---
+++ test.js
@@ -0,0 +1,25 @@
+// Import the function sum from the app.js file
+const { sum } = require('./app.js');
+
+// Start your first test
+test('adds 14 + 9 to equal 23', () => {
+    let total = sum(14, 9);
+
+    // We expect the sum of those 2 numbers to be 23
+    expect(total).toBe(23);
+});
+
+test("One euro should be 1.07 dollars", function() {
+    // Import the function from app.js
+    const { fromYenToPound } = require('./app.js');
+
+    // Use the function like its supposed to be used
+    const dollars = fromYenToPound(3.5);
+
+    // If 1 euro is 1.07 dollars, then 3.5 euros should be (3.5 * 1.07)
+    const expected = 3.5 * 0.87; 
+    
+    // This is the comparison for the unit test
+    expect(dollars).toBe (3.5 * 0.87)
+    // expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
+})
